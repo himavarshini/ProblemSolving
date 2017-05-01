1348ffca20b240743d54c97d87b1d1448555b23f
himavarshini <himayarragangu@gmail.com>
2017-05-01 21:19:16 +0000

    Prims algorithm MST

--- MSTPrims.cpp
+++ MSTPrims.cpp
@@ -5,17 +5,18 @@ struct edge{
   int v;
   int w;
 };
+
 int main(){
-  int n,e;
+  int n,e,dis=0;
   cin>>n>>e;
-  int vis[e];
+  int vis[n+1];
   memset(vis,0,sizeof(vis));
   edge g[e];
   for(int i=0;i<e;i++){
     cin>>g[i].u>>g[i].v>>g[i].w;
   }
   for(int i=0;i<e;i++){
-    for(int j=0;j<=e-j;j++){
+    for(int j=0;j<e-j;j++){
       if(g[j].w>g[j+1].w){
         swap(g[j].u,g[j+1].u);
         swap(g[j].v,g[j+1].v);
@@ -23,9 +24,17 @@ int main(){
       }
     }
   }
+vis[g[0].u]=1;
   for(int i=0;i<e;i++){
-    int cu=g[i].u,cv=g[i].v;
-    if(vis[cu] && )
-  }
+    for(int j=0;j<e;j++ ){
+      int cu = g[j].u, cv=g[j].v;
+    if((vis[cu]==1 || vis[cv]==1) && (vis[cu]!= vis[cv])){
+      dis+=g[j].w;
+      vis[cu]=1;vis[cv]=1;
+        j=0;
+      }
+    }
+   }
+  cout<<dis<<endl;
   return 0;
 }
