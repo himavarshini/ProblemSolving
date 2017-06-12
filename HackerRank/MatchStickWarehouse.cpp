7960bee9eade4f6e1171ee360d225ed93e2a9ee1
Himavarshini Yarragangu <himayarragangu@gmail.com>
2017-06-12 11:55:18 +0000

    https://www.hackerrank.com/contests/codeagon/challenges/matchstick-warehouse-thief

---
+++ HackerRank/MatchStickWarehouse.cpp
@@ -0,0 +1,33 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+struct Crate{
+    int boxes;
+    int sticks;
+};
+bool compare(Crate n1,Crate n2){
+  return n1.sticks<n2.sticks; }
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int c;
+    long long n;
+    cin>>n>>c;
+    Crate cr[c];
+    for(int i=0;i<c;i++){
+        cin>>cr[i].boxes>>cr[i].sticks;
+    }
+    sort(cr,cr+c,compare);
+    long long tn=n,ans=0;
+    for(int i=0;tn>0;i++){
+        if(tn>=cr[i].boxes)
+        ans+=cr[i].sticks*(cr[i].boxes);
+        else
+            ans+=cr[i].sticks*tn;
+        tn-=cr[i].boxes;
+    }
+    cout<<ans<<endl;
+    return 0;
+}
